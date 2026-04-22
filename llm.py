c6a8b9181bcae5618f24038205e446c66bf2b6a9
Vaibhav M <vaibhavm99@outlook.com>
2026-04-22 17:40:51 +0530

    llm.py

--- llm.py
+++ llm.py
@@ -1,10 +1,11 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = st.get_secrets["GEMINI_API_KEY"]
 
 llm = ChatGoogleGenerativeAI(
     model='gemini-2.5-flash-lite'
